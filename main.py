224aeca59ef4355db033864b84fd35755fadf466
kevin504-max <kevinbrito2012@gmail.com>
2024-09-26 19:55:20 -0300

    refactor: include closest neiborhood and 2-opt heuristic

--- main.py
+++ main.py
@@ -51,17 +51,56 @@ orig_node = ox.distance.nearest_nodes(G, orig_lon, orig_lat)
 num_carteiros = int(input("Insira o número de carteiros (n): "))
 rotas = [[] for _ in range(num_carteiros)]
 
-# Implementa uma heurística construtiva
-def heuristica_construtiva(G, orig_node, poi_coords, num_carteiros):
+# Implementa a heurística do vizinho mais próximo
+def heuristica_vizinho_mais_proximo(G, orig_node, poi_coords, num_carteiros):
     remaining_pois = poi_coords.copy()
-    # Distribui os POIs entre os carteiros
-    for i in range(len(remaining_pois)):
-        carteiro_index = i % num_carteiros  # Alterna entre os carteiros
-        rotas[carteiro_index].append(remaining_pois[i])
+    for carteiro_index in range(num_carteiros):
+        current_node = orig_node
+        route = []
+        while remaining_pois:
+            # Encontra o POI mais próximo
+            distances = []
+            for poi in remaining_pois:
+                poi_node = ox.distance.nearest_nodes(G, poi[1], poi[0])
+                distance = nx.shortest_path_length(G, current_node, poi_node, weight='length')
+                distances.append((distance, poi))
+            # Seleciona o POI mais próximo
+            next_poi = min(distances, key=lambda x: x[0])[1]
+            route.append(next_poi)
+            remaining_pois.remove(next_poi)
+            current_node = ox.distance.nearest_nodes(G, next_poi[1], next_poi[0])  # Atualiza o nó atual
+        rotas[carteiro_index] = route
     return rotas
 
-# Gera as rotas usando a heurística construtiva
-rotas = heuristica_construtiva(G, orig_node, poi_coords, num_carteiros)
+# Implementa a heurística de refinamento 2-opt
+def two_opt(route, G):
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(route) - 1):
+            for j in range(i + 1, len(route)):
+                if j - i == 1:  # Não permitir troca de dois nós adjacentes
+                    continue
+                # Calcula o ganho de distância
+                current_distance = (
+                    nx.shortest_path_length(G, ox.distance.nearest_nodes(G, route[i-1][1], route[i-1][0]), ox.distance.nearest_nodes(G, route[i][1], route[i][0]), weight='length') +
+                    nx.shortest_path_length(G, ox.distance.nearest_nodes(G, route[j-1][1], route[j-1][0]), ox.distance.nearest_nodes(G, route[j][1], route[j][0]), weight='length'))
+                new_distance = (
+                    nx.shortest_path_length(G, ox.distance.nearest_nodes(G, route[i-1][1], route[i-1][0]), ox.distance.nearest_nodes(G, route[j][1], route[j][0]), weight='length') +
+                    nx.shortest_path_length(G, ox.distance.nearest_nodes(G, route[i][1], route[i][0]), ox.distance.nearest_nodes(G, route[j-1][1], route[j-1][0]), weight='length'))
+                # Se a nova distância for menor, aplica a troca
+                if new_distance < current_distance:
+                    route[i:j+1] = reversed(route[i:j+1])
+                    improved = True
+    return route
+
+# Gera as rotas usando a heurística do vizinho mais próximo
+rotas = heuristica_vizinho_mais_proximo(G, orig_node, poi_coords, num_carteiros)
+
+# Refina as rotas usando o algoritmo 2-opt
+for i in range(num_carteiros):
+    print(f"Refinando a rota do carteiro {i + 1} com 2-opt...")
+    rotas[i] = two_opt(rotas[i], G)
 
 # Plota todas as rotas para cada carteiro com cores diferentes
 colors = plt.cm.viridis(np.linspace(0, 1, num_carteiros))  # Gera uma paleta de cores
@@ -102,7 +141,7 @@ for i, rota in enumerate(rotas):
 for route, color in all_routes:
     print("Plotando o grafo e destacando as rotas...") 
     ox.plot_graph_route(G, route, route_linewidth=6, node_size=0, ax=ax, edge_color=color)
-    
-plt.title(f"Rotas dos Carteiros")
-plt.show()
+
+# plt.title(f"Rotas dos Carteiros")
+# plt.show()
 print("Processo concluído!")
